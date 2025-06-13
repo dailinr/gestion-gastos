8f6e55369cb188e40b9cf77fad830ab763dd2661
Dailin Romero <dailinromero123@gmail.com>
2025-06-13 18:05:43 -0500

    mejora nav movil

--- dashboard_gastos/src/components/MovileNav.tsx
+++ dashboard_gastos/src/components/MovileNav.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom"
 import { LayoutDashboard, ChartColumnBig, BanknoteArrowDown } from "lucide-react"
-import { Button } from "./ui/button"
 
 // Reutilizamos la misma configuración de items
 const items = [
@@ -26,24 +25,20 @@ export function MobileNav() {
     // Contenedor fijo en la parte inferior de la pantalla
     <nav className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t">
       {/* Grid para distribuir los elementos equitativamente */}
-      <div className="grid h-full grid-cols-3 mx-auto">
+      <div className="grid h-full grid-cols-3 ">
         {items.map((item) => (
           <NavLink
-            key={`mobile-${item.title}`}
-            to={item.url}
-            className="inline-flex flex-col items-center justify-center "
-            end
+            key={`mobile-${item.title}`} to={item.url}
+            className=" inline-flex justify-center items-center" end
           >
 
             {({ isActive }) => (
-              <Button variant="ghost" 
-                className={`flex flex-col items-center justify-center hover:bg-muted ${
-                  isActive ? "text-primary" : "text-muted-foreground"
-                }`}
+              <div className={` flex flex-col p-2 rounded-md items-center
+                ${ isActive ? "text-primary bg-muted" : "text-muted-foreground"}`}
               >
-                <item.icon className="w-5 h-5" />
+                <item.icon className="w-6 h-6" />
                 <span className="text-xs">{item.title}</span>
-              </Button>
+              </div>
             )}
           </NavLink>
         ))}
